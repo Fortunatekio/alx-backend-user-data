268e30b8d7a1c31abb881d1d3c4434c0586902a9
Fortunatekio <fortunatemugendi8@gmail.com>
2024-04-12 01:38:31 +0300

    filtered

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -36,8 +36,8 @@ def filter_datum(fields: List[str], redaction: str, message: str,
 
     for i in fields:
         message = re.sub(i + "=.*?" + separator,
-                i + "=" + redaction + separator,
-                message)
+                         i + "=" + redaction + separator,
+                         message)
     return message
 
 
@@ -61,12 +61,12 @@ class RedactingFormatter(logging.Formatter):
         """ Constructor Method """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
-    
+
     def format(self, record: logging.LogRecord) -> str:
         """ Filters values in incoming log records using filter_datum """
         return filter_datum(self.fields, self.REDACTION,
-                super(RedactingFormatter, self).format(record),
-                self.SEPARATOR)
+                            super(RedactingFormatter, self).format(record),
+                            self.SEPARATOR)
 
 
 def get_logger() -> logging.Logger:
